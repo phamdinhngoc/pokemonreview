dcaccdc7a6beec41f10ef2e4fb3489f55da5e008
unknown <phamdinhngoc86@gmail.com>
2023-05-12 08:23:37 +0700

    initial commit

---
+++ PokemonReviewApp/Models/Country.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Owner> Owners { get; set; } //Một country có nhiều Owners
+    }
+}
---
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -0,0 +1,14 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public ICollection<Review> Reviews { get; set; } //liên kết 1-nhiêu với bảng Review 1 pokemon sẽ có nhiều review
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+        public ICollection<PokemonCategory> PokemonCategories { get; set; }
+
+    }
+}
